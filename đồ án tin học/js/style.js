0814f49a38c7eb157b228bf5410a08e572e08ea1
ADTienGiang <88886763+ADTienGiang@users.noreply.github.com>
2021-12-19 13:45:37 +0700

    upload

---
+++ đồ án tin học/js/style.js
@@ -0,0 +1,28 @@
+/*******************************
+* ACCORDION WITH TOGGLE ICONS
+*******************************/
+function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
+
+
+
+// smooth page scroll.
+
+/*
+	Smooth scroll functionality for anchor links (animates the scroll
+	rather than a sudden jump in the page)
+*/
+$('.js-anchor-link').click(function(e){
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    if(target.length){
+      var scrollTo = target.offset().top;
+      $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
+    }
+  });
